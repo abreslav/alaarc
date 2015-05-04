8ff9efe450813d91af123dcde0561a780fd4afae
Andrey Breslav <andrey.breslav@jetbrains.com>
2015-05-04 23:28:13 +0300

    .idea

---
+++ abreslav-tests/first.asm
@@ -0,0 +1,64 @@
+Global variables: 
+  #0:c
+  #1:a
+  #2:b
+
+Thread #0:
+  createObject @abreslav-tests/first.txt:1
+  storeGlobal a @abreslav-tests/first.txt:1
+  loadGlobal a @abreslav-tests/first.txt:2
+  dump @abreslav-tests/first.txt:2
+  createObject @abreslav-tests/first.txt:3
+  loadGlobal a @abreslav-tests/first.txt:3
+  storeSlot b @abreslav-tests/first.txt:3
+  loadGlobal a @abreslav-tests/first.txt:4
+  dump @abreslav-tests/first.txt:4
+  loadGlobal a @abreslav-tests/first.txt:5
+  loadSlot b @abreslav-tests/first.txt:5
+  dump @abreslav-tests/first.txt:5
+  loadGlobal a @abreslav-tests/first.txt:6
+  storeWeakGlobal b @abreslav-tests/first.txt:6
+  loadGlobal b @abreslav-tests/first.txt:7
+  dump @abreslav-tests/first.txt:7
+  releaseGlobal b @abreslav-tests/first.txt:7
+  loadGlobal a @abreslav-tests/first.txt:9
+  loadGlobal a @abreslav-tests/first.txt:9
+  storeSlot b @abreslav-tests/first.txt:9
+  loadGlobal a @abreslav-tests/first.txt:10
+  dump @abreslav-tests/first.txt:10
+  loadGlobal a @abreslav-tests/first.txt:11
+  loadGlobal a @abreslav-tests/first.txt:11
+  storeSlot b @abreslav-tests/first.txt:11
+  loadGlobal a @abreslav-tests/first.txt:12
+  dump @abreslav-tests/first.txt:12
+  loadGlobal a @abreslav-tests/first.txt:13
+  loadGlobal a @abreslav-tests/first.txt:13
+  storeSlot b @abreslav-tests/first.txt:13
+  loadGlobal a @abreslav-tests/first.txt:14
+  dump @abreslav-tests/first.txt:14
+  loadGlobal a @abreslav-tests/first.txt:15
+  loadGlobal a @abreslav-tests/first.txt:15
+  storeSlot b @abreslav-tests/first.txt:15
+  loadGlobal a @abreslav-tests/first.txt:16
+  dump @abreslav-tests/first.txt:16
+  loadGlobal a @abreslav-tests/first.txt:17
+  loadGlobal a @abreslav-tests/first.txt:17
+  storeSlot b @abreslav-tests/first.txt:17
+  loadGlobal a @abreslav-tests/first.txt:18
+  dump @abreslav-tests/first.txt:18
+  createObject @abreslav-tests/first.txt:19
+  loadGlobal a @abreslav-tests/first.txt:19
+  storeSlot b @abreslav-tests/first.txt:19
+  createObject @abreslav-tests/first.txt:20
+  storeGlobal c @abreslav-tests/first.txt:20
+  loadGlobal a @abreslav-tests/first.txt:21
+  loadGlobal c @abreslav-tests/first.txt:21
+  storeSlot a @abreslav-tests/first.txt:21
+  pushNull  @abreslav-tests/first.txt:22
+  storeGlobal a @abreslav-tests/first.txt:22
+  releaseGlobal a @abreslav-tests/first.txt:22
+  loadGlobal c @abreslav-tests/first.txt:23
+  loadSlot a @abreslav-tests/first.txt:23
+  dump @abreslav-tests/first.txt:23
+  releaseGlobal c @abreslav-tests/first.txt:23
+
